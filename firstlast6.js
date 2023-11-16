f2093cd41c1877821ed9884f9ea93dc44f2766a5
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-16 06:53:42 +0100

    update

---
+++ firstlast6.js
@@ -0,0 +1,15 @@
+Array-1 -- firstLast6
+Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+Examples
+
+firstLast6([1, 2, 6]) → true
+firstLast6([6, 1, 2, 3]) → true
+firstLast6([13, 6, 1, 2, 3]) → false
+
+function firstLast6(nums){
+    let len = nums.length;
+      if(nums[0] === 6 || nums[len-1] === 6 ){
+      return true
+      }return false
+  }console.log(firstLast6([1, 2, 6]));
